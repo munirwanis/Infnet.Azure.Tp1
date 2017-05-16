4d004533b5d8af47df902c66fb184d7841da2d07
Munir Wanis <fallen.angel.pp@gmail.com>
2017-05-16 12:11:15 -0300

    First Commit

---
+++ Infnet.Azure.Tp1.Data/ContactBlobStorageDAO.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace Infnet.Azure.Tp1.Data
+{
+    public class ContactBlobStorageDAO
+    {
+        private string ContainerReference { get; } = "munirwanis";
+        private CloudStorageAccount StorageConnectionString { get; } = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+        public void AddBlob(string path)
+        {
+            Console.WriteLine("Loading blob to Storage");
+            CloudStorageAccount storageAccount = this.StorageConnectionString;
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(this.ContainerReference);
+
+            container.CreateIfNotExists();
+
+            var pathSplit = path.Split('\\');
+
+            var nomeDoArquivo = pathSplit[pathSplit.Length - 1];
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(nomeDoArquivo);
+
+            using (var fileStream = System.IO.File.OpenRead(path))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+            Console.WriteLine("Successfuly loaded file on Storage");
+        }
+
+        public void DeleteBlob()
+        {
+            Console.WriteLine("Which file do you want to delete?");
+            string nomeDoArquivo = Console.ReadLine();
+
+            CloudStorageAccount storageAccount = this.StorageConnectionString;
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(this.ContainerReference);
+
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(nomeDoArquivo);
+            if (blockBlob != null)
+            {
+                blockBlob.DeleteIfExists();
+                Console.WriteLine("Successfuly deleted file from Storage");
+
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+
+        }
+
+        public void DeleteContainer()
+        {
+            Console.WriteLine("Deleting container from Storage");
+
+            CloudStorageAccount storageAccount = this.StorageConnectionString;
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(this.ContainerReference);
+
+            container.DeleteIfExists();
+            Console.WriteLine("Successfuly deleted Container from Storage");
+
+
+        }
+    }
+}
